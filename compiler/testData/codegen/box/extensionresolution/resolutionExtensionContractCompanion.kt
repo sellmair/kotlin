7e2b217896227ebb89e7a1448da8276ed9a9e5ce
Tomás Ruiz-López <truizlop@gmail.com>
2019-04-09 10:23:26 +0200

    Update test for resolution in companion object of extension contract

--- compiler/testData/codegen/box/extensionresolution/resolutionExtensionContractCompanion.kt
+++ compiler/testData/codegen/box/extensionresolution/resolutionExtensionContractCompanion.kt
@@ -1,24 +1,27 @@
 // TARGET_BACKEND: JVM
 // WITH_RUNTIME
-// FILE: Semigroup.kt
+// FILE: Validator.kt
 
 package com.extensionresolution
 
-interface Semigroup<A> {
-    fun A.combine(b: A): A
+interface Validator<A> {
+    fun A.isValid(): Boolean
 
     companion object {
-        extension object IntSemigroup : Semigroup<Int> {
-            override fun Int.combine(b: Int): Int = this + b
+        extension object UserValidator: Validator<User> {
+            override fun User.isValid(): Boolean {
+                return id > 0 && name.length > 0
+            }
         }
     }
 }
 
-fun <A> duplicate(a: A, with semigroup: Semigroup<A>) : A = a.combine(a)
+data class User(val id: Int, val name: String)
+
+fun <A> validate(a: A, with validator: Validator<A>): Boolean = a.isValid()
 
 fun box(): String {
-    val x = duplicate(2)
-    return if (x == 4) {
+    return if (validate(User(1, "Alice"))) {
         "OK"
     } else {
         "fail 1"

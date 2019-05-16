441498985d10625fc87ccbb3845785879e063a03
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-05-16 11:48:02 +0300

    Introduce experimental FIR compiler #KT-31265 Fixed
    This commit also includes several FIR2IR fixes which helps FIR compiler
    to produce normal results

--- build.gradle.kts
+++ build.gradle.kts
@@ -245,7 +245,8 @@ extra["compilerModules"] = arrayOf(
         ":compiler:fir:resolve",
         ":compiler:fir:tree",
         ":compiler:fir:psi2fir",
-        ":compiler:fir:fir2ir"
+        ":compiler:fir:fir2ir",
+        ":compiler:fir:java"
 )
 
 val coreLibProjects = listOfNotNull(
